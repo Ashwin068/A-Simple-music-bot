07235b22e136edf94842387777ad97a3a07f35ec
TRIVIA TIME <52164560+TRIVIATIME@users.noreply.github.com>
2019-11-30 18:40:08 +0530

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -5,7 +5,7 @@ module.exports.run = async (bot, message, args) => {
   let embed = new discord.RichEmbed()
     .setTitle('MUSIC COMMAND')
   .setURL(bot.user.displayAvatarURL)
-  .setDescription('**;play/;p**\nUse it like`;play<songname/url>`\n**;search**\nUse it like `;search <songname>`\n**;np/;nowplaying**\nCheck Who play song now\n**;pause**\npause your play current song')
+  .setDescription('**;play/;p**\nUse it like`;play<songname/url>`\n**;search**\nUse it like `;search <songname>`\n**;np/;nowplaying**\nCheck Who play song now\n**;pause**\n`pause your play current song`\n**;resume**\n`if song pause then use it`\n**;skip/;s/;next/;nxt**\n`Next song of queye list`\n**;dis/;dc**\n`if you want stop song`\n')
   .setAuthor('TRIVIA TIME')
   .setThumbnail(bot.user.displayAvatarURL)
   .setColor("RANDOM")
