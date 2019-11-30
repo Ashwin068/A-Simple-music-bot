635bcbd450cf5463dc0db592eafb96f68594f096
TRIVIA TIME <52164560+TRIVIATIME@users.noreply.github.com>
2019-11-30 11:25:52 +0530

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -4,7 +4,7 @@ const { prefix } = require("../settings/config.json");
 module.exports.run = async (bot, message, args) => {
   let embed = new discord.RichEmbed()
     .setTitle('MUSIC COMMAND')
-  .setURL('(bot.user.displayAvatarURL')
+  .setURL(bot.user.displayAvatarURL)
   .setDescription('**;play**\nUse it like`;play<songname/url>`\n**;search**\nUse it like `;search <songname>`')
   .setAuthor('TRIVIA TIME')
   .setThumbnail(bot.user.displayAvatarURL)
